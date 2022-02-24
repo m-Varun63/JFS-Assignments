f842f6db0ff3a1c41421d6cadf413ef551bef770
m-Varun63 <maheshwarivarun63@gmail.com>
2022-02-24 10:41:12 +0530

    created lambda

---
+++ Core Java/Lamda/src/Q5.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+
+
+public class Q5 {
+    List<String> list = Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+
+    public static void main(String[] args) {
+        Q5 assignment4Q5 = new Q5();
+        processWords(assignment4Q5.list);
+    }
+
+    public static String processWords(List<String> list) {
+        StringBuilder words = new StringBuilder();
+        Consumer<String> consumer = p-> words.append(p.charAt(0));
+
+         list.stream().forEach(consumer);
+        System.out.println(words);
+        return  words.toString();
+    }
+}
