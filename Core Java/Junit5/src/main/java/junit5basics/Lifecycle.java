ddb29b41597ad39941f5551fa8b8877ae651b8fc
m-Varun63 <maheshwarivarun63@gmail.com>
2022-03-04 09:42:34 +0530

    created junit

---
+++ Core Java/Junit5/src/main/java/junit5basics/Lifecycle.java
@@ -0,0 +1,11 @@
+package junit5basics;
+
+public class Lifecycle {
+    public static int multi(int a, int b){
+        return a * b;
+    }
+    public static int div(int a, int b){
+        return a / b;
+    }
+
+}
