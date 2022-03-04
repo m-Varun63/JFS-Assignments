b85638b3ac2d60a710e4a5c622a50a0be6f62faf
m-Varun63 <maheshwarivarun63@gmail.com>
2022-03-04 10:08:35 +0530

    created junit

---
+++ Junit/Junit5/src/main/java/Lifecycle.java
@@ -0,0 +1,9 @@
+public class Lifecycle {
+	  public static int multi(int a, int b){
+	        return a * b;
+	    }
+	    public static int div(int a, int b){
+	        return a / b;
+	    }
+
+	}
---
+++ Junit/Junit5/src/test/java/LifecycleTest.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+class LifecycleTest {
+	int a = 5;
+    int b = 5;
+    @BeforeAll
+    static void printd(){
+        System.out.println("started test case");
+    }
+    @AfterEach
+    void printc(){
+        System.out.println("completed test case");
+    }
+    @AfterAll
+    static void printall(){
+        System.out.println("Finished printing");
+    }
+    @BeforeEach
+    void setUp() {
+        System.out.println("Executing all");
+    }
+    @Test
+  public  void multi() {
+        assertEquals(25,Lifecycle.multi(a,b));
+    }
+
+    @Test
+  public  void div() {
+        assertEquals(1,Lifecycle.div(a,b));
+
+	}
+
+}
