671f8a2d8166bb4e504e0360039aabf01022617f
Varun Maheshwari <70843454+m-Varun63@users.noreply.github.com>
2022-03-23 10:22:28 +0530

    Delete SpringREST directory

--- SpringREST/src/main/java/com/springrest/Q7/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.Q7;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepo extends MongoRepository<Order,String>
-{
-}
