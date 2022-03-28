802d161787c015da51f024b854fea463659e5bf2
m-Varun63 <maheshwarivarun63@gmail.com>
2022-03-28 11:07:36 +0530

    created q3

---
+++ SpringSecurity/A1/SpringSecurityQ3/src/main/java/com/springsecurity/SpringSecurityQ3Application.java
@@ -0,0 +1,13 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityQ3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityQ3Application.class, args);
+	}
+
+}
