631c367c4916e4f309dfa77e82aa75d96731db57
Varun Maheshwari <70843454+m-Varun63@users.noreply.github.com>
2022-04-26 10:33:13 +0530

    uploaded level 3

---
+++ SpringCloud/Microservice Level 3/spring-boot-config/src/main/java/com/microservice/springbootconfig/GreetingController.java
@@ -0,0 +1,49 @@
+package com.microservice.springbootconfig;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class GreetingController {
+	
+	@Value("${my.greeting: default value}")
+	private String greetingMessage;
+	
+	@Value("some static value")
+	private String staticMessage;
+	
+	@Value("${my.list.values}")
+	private List<String> listValues;
+	
+//	@Value("#{${dbValues}}")
+//	private Map<String, String> dbValues;
+	
+//	@Autowired
+//	private DbSettings dbSettings;
+	
+//	@Autowired
+//	private Environment env;
+	
+//	public GreetingController() {
+//	}
+	
+	@GetMapping("/greeting")
+	public String greeting( ) {
+		return greetingMessage + staticMessage + listValues; //+ dbValues;
+//		return dbSettings.getConnection() + dbSettings.getHost();
+	}
+	
+//	@GetMapping("/envdetails")
+//	public String envDetails() {
+//		return env.toString();
+//	}
+
+}
